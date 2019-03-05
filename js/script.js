2fff8fce8dd5c8a325aeadf052270dad97454154
Monika Tadross <monikk.tadross@gmail.com>
2019-03-05 21:01:25 +0000

    Automatically backed up by Learn

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
   $(document).ready(function() {
   $("#text").append("this is so freaking awesome!!!.");
   });
+=======
+/*$(document).ready(function() {
+  console.log('Readyyyy!!!');
+}); */
+>>>>>>> 562c634d2bd8989ac408ea54071b2d7616153bb8
